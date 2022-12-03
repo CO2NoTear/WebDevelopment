acec3cc373f50a858af17c7e0b06d9c28ef4097a
FleurDeLysFDl <34373063+FleurDeLysFDl@users.noreply.github.com>
2022-12-03 21:59:25 +0800

    Add files via upload

---
+++ detailed files/JS/include.js
@@ -0,0 +1,8 @@
+$(function () {
+    $.get("../include/header.html",function (data) {
+        $("#header").html(data);
+    });
+    $.get("footer.html",function (data) {
+        $("#footer").html(data);
+    });
+});
